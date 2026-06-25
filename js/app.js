42a38616ecb7207311bf393e4a7c29c7e0e5f27b
jglim17 <33038464+jglim17@users.noreply.github.com>
2026-06-25 11:07:19 +0200

    Actualizar app.js

--- js/app.js
+++ js/app.js
@@ -1,31 +1,8 @@
-const objetivo = new Date("2026-07-06T12:00:00");
 
-function actualizarCuentaAtras() {
-    const ahora = new Date();
-    const diferencia = objetivo - ahora;
-
-    const contador = document.getElementById("cuenta-atras");
-
-    if (diferencia <= 0) {
-        contador.innerHTML =
-`⏳ Faltan ${dias} días, ${horas} horas, ${minutos} minutos y ${segundos} segundos`;
-        return;
-    }
-
-    const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
-    const horas = Math.floor((diferencia / (1000 * 60 * 60)) % 24);
-    const minutos = Math.floor((diferencia / (1000 * 60)) % 60);
-    const segundos = Math.floor((diferencia / 1000) % 60);
-
-    contador.innerHTML =
-        `⏳ ${dias}d ${horas}h ${minutos}m ${segundos}s`;
-}
-
-actualizarCuentaAtras();
-setInterval(actualizarCuentaAtras, 1000);
 
 async function cargarConciertos() {
     document.getElementById("proximos").innerHTML = "Estoy ejecutando app.js";
 }
 
 cargarConciertos();
+document.getElementById("proximos").innerHTML = "app.js cargado";
