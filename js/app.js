aaeb7c1b9a7c219603379fbd272a918fe5a546f9
jglim17 <33038464+jglim17@users.noreply.github.com>
2026-06-25 11:23:21 +0200

    Actualizar app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,28 @@
 async function cargarConciertos() {
     const div = document.getElementById("proximos");
 
-    const respuesta = await fetch("data/conciertos.json");
-    const conciertos = await respuesta.json();
+    try {
+        const respuesta = await fetch("data/conciertos.json");
+        const conciertos = await respuesta.json();
 
-    div.innerHTML = JSON.stringify(conciertos);
+        let html = "";
+
+        conciertos.forEach(concierto => {
+            html += `
+                <div class="concierto">
+                    <h3>${concierto.artista}</h3>
+                    <p>🕒 ${concierto.hora}</p>
+                    <p>📍 ${concierto.escenario}</p>
+                </div>
+            `;
+        });
+
+        div.innerHTML = html;
+
+    } catch (error) {
+        div.innerHTML = "❌ Error cargando conciertos";
+        console.error(error);
+    }
 }
+
 cargarConciertos();
