7e461cca7672fbdf0e627b788200aea1902fdbd2
jglim17 <33038464+jglim17@users.noreply.github.com>
2026-06-25 11:10:50 +0200

    Actualizar app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,30 @@
-
-
 async function cargarConciertos() {
-    document.getElementById("proximos").innerHTML = "Estoy ejecutando app.js";
+    const div = document.getElementById("proximos");
+
+    try {
+        const respuesta = await fetch("data/conciertos.json");
+
+        if (!respuesta.ok) {
+            throw new Error("No se pudo cargar el archivo.");
+        }
+
+        const conciertos = await respuesta.json();
+
+        div.innerHTML = "";
+
+        conciertos.forEach(concierto => {
+            div.innerHTML += `
+                <div class="concierto">
+                    <strong>${concierto.hora}</strong> - ${concierto.artista}<br>
+                    <small>${concierto.escenario}</small>
+                </div>
+            `;
+        });
+
+    } catch (error) {
+        div.innerHTML = "❌ Error cargando conciertos.";
+        console.error(error);
+    }
 }
 
 cargarConciertos();
-document.getElementById("proximos").innerHTML = "app.js cargado";
