3bb240064beda961107df46e916cf08594f0711e
jglim17 <33038464+jglim17@users.noreply.github.com>
2026-06-25 11:32:50 +0200

    Actualizar app.js

--- js/app.js
+++ js/app.js
@@ -1,36 +1,22 @@
+alert("APP NUEVA");
+
 async function cargarConciertos() {
     const proximos = document.getElementById("proximos");
 
-    try {
-        const respuesta = await fetch("data/conciertos.json");
+    const respuesta = await fetch("data/conciertos.json");
+    const conciertos = await respuesta.json();
 
-        if (!respuesta.ok) {
-            throw new Error("No se pudo cargar conciertos.json");
-        }
+    proximos.innerHTML = "";
 
-        const conciertos = await respuesta.json();
-
-        proximos.innerHTML = "";
-
-        conciertos.forEach(concierto => {
-
-            const tarjeta = document.createElement("div");
-            tarjeta.className = "concierto";
-
-            tarjeta.innerHTML = `
+    conciertos.forEach(concierto => {
+        proximos.innerHTML += `
+            <div class="concierto">
                 <h3>${concierto.artista}</h3>
                 <p>🕒 ${concierto.hora}</p>
                 <p>📍 ${concierto.escenario}</p>
-            `;
-
-            proximos.appendChild(tarjeta);
-
-        });
-
-    } catch (error) {
-        console.error(error);
-        proximos.innerHTML = `<p>${error.message}</p>`;
-    }
+            </div>
+        `;
+    });
 }
 
 document.addEventListener("DOMContentLoaded", cargarConciertos);
